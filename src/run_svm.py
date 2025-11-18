01054c3f808f89f925cf64e8278ebc5ccf21f9fd
Radhika Khurana <mail.radhikakhurana@gmail.com>
2025-11-17 23:50:53 -0500

    Create run_svm.py

---
+++ src/run_svm.py
@@ -0,0 +1,23 @@
+from data_preprocessing import load_data, preprocess_data, split_data
+from model_training import build_models, train_model
+from evaluate_models import evaluate, save_results
+
+# 1. Data
+df = load_data("Training_Dataset.csv")
+X_res, y_res = preprocess_data(df)
+X_train, X_test, y_train, y_test = split_data(X_res, y_res, test_size=0.2)
+
+# 2. Build models
+all_models = build_models()
+metrics_list = []
+
+model = all_models["SVM (RBF)"]
+# 3. Train (with tuning) & save
+best_model = train_model(model, X_train, y_train, name)
+
+# 4. Evaluate
+metrics = evaluate(best_model, X_test, y_test, name)
+metrics_list.append(metrics)
+
+# 5. Save metrics summary (optional)
+save_results(metrics_list)
